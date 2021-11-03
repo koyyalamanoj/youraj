a36e376b85084726aa983c651ce04ed6ece1d694
prashanthsonu <43454911+prashanthsonu@users.noreply.github.com>
2021-11-02 21:06:57 -0600

    hello manoj

---
+++ src/main/java/Vijay/Youraj/App.java
@@ -0,0 +1,13 @@
+package Vijay.Youraj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World manoj!" );
+    }
+}
